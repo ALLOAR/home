e78aaa85368695efa42243460449ed2a62505fee
ALLOAR <you@example.com>
2025-06-08 23:23:43 +0200

    more bautiful script and work everywhere

--- config/systemd/user/swww/swww.sh
+++ config/systemd/user/swww/swww.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-wallpaper=$((1 + RANDOM % 6))
+wallpaper=$((1 + RANDOM % 8))
 echo $wallpaper
-swww img ~/home/project/scripts/$wallpaper.jpeg --transition-type any
+swww img /home/alloar/.config/systemd/user/swww/$wallpaper.jpeg --transition-type any

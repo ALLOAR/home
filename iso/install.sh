fe8d7d49d923149616a0b91efab483121157eca2
ALLOAR <you@example.com>
2025-06-16 21:22:54 +0200

    added options for nvme disk

--- iso/install.sh
+++ iso/install.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 set -e
-
+lsblk
+read -p " --- if you use sata interface just press ENTER if nvme sdd pres 'p') --- " p
 echo "[1/5] Разметка $DISK..."
 DISK="/dev/sda"  # ❗ Измени только это
 
@@ -16,18 +17,18 @@ parted $DISK -- mkpart primary linux-swap 512MiB 8704MiB
 parted $DISK -- mkpart primary ext4 8704MiB 100%
 echo "[2/5] Форматирование разделов..."
 # Форматирование EFI
-mkfs.fat -F32 -n boot ${DISK}1
+mkfs.fat -F32 -n boot ${DISK}${p}1
 # Форматирование swap
-mkswap -L swap ${DISK}2
+mkswap -L swap ${DISK}${p}2
 # Форматирование ext4
-mkfs.ext4 -L nixos ${DISK}3
+mkfs.ext4 -L nixos ${DISK}${p}3
 echo "[3/5] Активация swap..."
-swapon ${DISK}2
+swapon ${DISK}${p}2
 
 echo "[3.5/5] Монтирование разделов..."
-mount ${DISK}3 /mnt
+mount ${DISK}${p}3 /mnt
 mkdir -p /mnt/boot
-mount ${DISK}1 /mnt/boot
+mount ${DISK}${p}1 /mnt/boot
 
 
 echo "[4/5] Копирование конфигов..."
@@ -39,7 +40,7 @@ cd NixOS
 cp -r * /mnt/etc/nixos/
 cp /mnt/etc/nixos/hardware-configuration.nix /mnt/etc/nixos/configurations/
 cd
-nix-channel --add https://nixos.org/channels/nixos-24.11 nixos nix-channel --update
+nix-channel --add https://nixos.org/channels/nixos-25.05 nixos nix-channel --update
 sudo nano /mnt/etc/nixos/configuration.nix
 echo "[5/5] Установка..."
 

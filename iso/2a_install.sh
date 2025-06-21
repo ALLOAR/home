8c07cde34bd9900083251d6b5fb7ed1fd3a1340a
ALLOAR <you@example.com>
2025-06-21 23:18:01 +0200

    no confirm package install

--- iso/2a_install.sh
+++ iso/2a_install.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 echo "arch" > /etc/hostname
-pacman -S grub efibootmgr
+pacman -S --noconfirm grub efibootmgr
 grub-install --target=x86_64-efi --bootloader-id=GRUB --efi-directory=/boot
 grub-mkconfig -o /boot/grub/grub.cfg
 echo "Dont forgot locales"
-passwd
+echo "root:root" | chpasswd
+pacman -S sudo

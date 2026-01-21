ae5e0c7e6093aaf917e823cdffe486ba641a7fcd
ALLOAR <evgenzbiranik@gmail.com>
2026-01-21 21:07:20 +0100

    ended configurationg script, this will be only chanche disk partition

--- install/disk.sh
+++ install/disk.sh
@@ -1,96 +1,157 @@
 #/usr/env/bin bash
 set -e
-function clean_disk_no_swap() {
-	echo " --- disk partitioning ---"
-	parted /dev/$DISK -- mkalbel gpt
-	parted /dev/$DiSK -- mkpart ESP fat32 1MiB 5GiB
-	parted /dev/$DISK -- set 1 boot on
-	parted /dev/$DISK -- mkpart primary ext4 5GiB 100%
-	echo "formatting partitions"
-	if [[ "$DISK" -eq "nvme0n1" ]]; then
-		mkfs.fat -F32 -n boot /dev/${DISK}p1
-		mkfs.ext4 /dev/${DISK}p2
-		mount ${DISK}p2 /mnt
-		mkdir -p /mnt/boot
-		mount ${DISK}p1 /mnt/boot
-	elif [[ "$DISK" -eq "sda" ]]; then
-		mkfs.fat -F32 -n boot /dev/${DISK}1
-                mkfs.ext4 /dev/${DISK}2
-		mount /dev/${DISK}2 /mnt
-		mkdir -p /mnt/boot
-		mount /dev/${DISK}1 /mnt/boot
-	else
-		echo " --- I dont know what disk you use (... --- "
-		exit 1
-	fi
+free_disksize_raw=$(parted /dev/$DISK unit GiB print free | grep "Free Space" | tail -n1 | awk '{print $3}')
+free_disksize=${free_disksize_raw%GiB}
+
+efi_size=$(lsblk -nb -o NAME,FSTYPE,FSSIZE,FSAVAIL /dev/$DISK | awk '$2 == "vfat" {print $4}')
+efi_size_echo=$(lsblk -n -o NAME,FSTYPE,FSSIZE,FSAVAIL /dev/$DISK | awk '$2 == "vfat" {print $4}'
+
+function last_part() {
+	echo $(( $(lsblk | grep $DISK | wc -l) - 1 ))
 }
-function clean_disk_with_swap() {
-	echo " --- disk partitioning ---"
-        parted /dev/$DISK -- mkalbel gpt
-        parted /dev/$DiSK -- mkpart ESP fat32 1MiB 5GiB
-        parted /dev/$DISK -- set 1 boot on
-	parted /dev/$DISK -- mkpart linux-swap 5GiB 8GiB
-        parted /dev/$DISK -- mkpart primary ext4 8GiB 100%
-        echo "formatting partitions"
-        if [[ "$DISK" -eq "nvme0n1" ]]; then
-                mkfs.fat -F32 -n boot /dev/${DISK}p1
-		mkswap -L swap /dev/${DISK}p2
-                mkfs.ext4 /dev/${DISK}p3
-		swapon /dev/${DISK}p2
-		mount ${DISK}p3 /mnt
-		mkdir -p /mnt/boot
-		mount ${DISK}p1 /mnt/boot
-        elif [[ "$DISK" -eq "sda" ]]; then
-                mkfs.fat -F32 -n boot /dev/${DISK}1
-                mkswap -L swap /dev/${DISK}2
-		mkfs.ext4 /dev/${DISK}3
-		swapon /dev/${DISK}2
-		mount /dev/${DISK}3 /mnt
-		mkdir -p /mnt/boot
-		mount /dev/${DISK}1 /mnt/boot
-        else
-                echo " --- I dont know what disk you use (... --- "
-                exit 1
-        fi
+function swap_part() {
+	echo $(( $(lsblk | grep $DISK | wc -l) - 2 ))
 }
-function dualboot_win_efi_no_swap () {
-	echo " --- disk partitioning ---"
-	count=$(lsblk | grep $DISK | wc -l)
-	b=1
-	(( primary_partition = count - b))
-	parted_start=${free_diskspace}GiB
-        parted /dev/$DISK -- mkalbel gpt
-        parted /dev/$DISK -- mkpart primary ext4 ${parted_start} 100%
-        echo "formatting partitions"
-        if [[ "$DISK" -eq "nvme0n1" ]]; then
-                mkfs.ext4 /dev/${DISK}p${primary_partition}
-                mount ${DISK}p${primary_partition} /mnt
+# usr/env/bin bash
+
+function clear_disk() {
+        local efi_end_raw
+        local efi_start
+        local efi_end
+        local swap_start
+        local swap_end
+        local last_partition
+        local swap_partition
+        local p=""
+
+	local free_disksize_raw
+	local efi_size
+
+	free_disksize_raw=$(parted /dev/$DISK unit GiB print free | grep "Free Space" | tail -n1 | awk '{print $3}')
+	free_disksize=${free_disksize_raw%GiB}
+	efi_size=$(lsblk -nb -o NAME,FSTYPE,FSSIZE,FSAVAIL /dev/$DISK | awk '$2 == "vfat" {print $4}')
+
+
+	read -p " --- we will erase all data yes/no (handwrite) --- \n --- " erase
+
+	[[ "$erase" == "no" ]] && echo " --- so try yourself --- " & exit 1
+
+        efi_start="1MiB"
+	read -p " --- how mush space you wanna for efi? (recomendet >2)--- \n --- you write only numer this will be in GiB --- \n --- " efi_end_raw
+        efi_end=${efi_end_raw}GiB
+
+	last_partition=$(last_part)
+        (( swap_partition=$(lsblk | grep $DISK | wc -l) - 2 ))
+
+        [[ "$DISK" == nvme* ]] && p="p"
+
+        parted /dev/$DISK -- mkpart ESP fat32 ${efi_start} ${efi_end}
+        efi_num=$(( $(lsblk | grep $DISK | wc -l) - 1) ))
+	parted -- set ${efi_num} boot on
+        mkfs.fat -F32 /dev/${DISK}${p}${efi_num}
+
+
+        if [[ "${swap}" -ne 0 ]]; then
+                swap_start=${efi_end}
+                swap_end=$(( ${swap} + ${efi_end_raw} ))
+                parted /dev/$DISK -- mkpart linux-swap ${swap_start} ${swap_end}
+                parted /dev/$DISK -- mkpart primary ext4 ${swap_end} 100%
+	        last_partition=$(last_part)
+                mkswap /dev/${DISK}${p}${last_partition}
+	        last_partition=$(last_part)
+                mkfs.ext4 /dev/${DISK}${p}${last_partition}
+		swap_partition=$(swap_part)
+		mkswap /dev/${DISK}${p}${swap_partition}
+                mount /dev/${DISK}${p}${last_partition} /mnt
                 mkdir -p /mnt/boot
-                mount ${DISK}p1 /mnt/boot
-        elif [[ "$DISK" -eq "sda" ]]; then
-		mkfs.ext4 /dev/${DISK}${primary_partition}
-                mount ${DISK}${primary_partition} /mnt
+                mount /dev/${DISK}${p}${efi_num} /mnt/boot
+
+        elif [[ "{$swap}"' -eq 0 ]]; then
+                parted /dev/$DISK -- mkpart primary ext4 ${efi_end} 100%
+	        last_partition=$(last_part)
+                mkfs.ext4 /dev/${DISK}${p}${last_partition}
+                mount /dev/${DISK}${p}${last_partition} /mnt
                 mkdir -p /mnt/boot
-                mount ${DISK}1 /mnt/boot
-	else
-                echo " --- I dont know what disk you use (... --- "
-                exit 1
+                mount /dev/${DISK}${p}${efi_num} /mnt/boot
+                echo " --- swap will not created --- "
         fi
 }
 
-lsblk
-read -p " --- what disk you will use? (1/2/3...) --- " disk_choise
+function dualboot() {
+        echo " --- disk partitioning ---"
+	local efi_start_raw
+	local efi_end_raw
+	local efi_start
+	local efi_end
+	local swap_start
+	local swap_end
+	local last_partition
+	local swap_partition
+        local p=""
+
+
+        read -p " --- how mush space you wanna for efi? (recomendet >2)--- \n --- you write only numer this will be in GiB --- \n --- " efi_end_raw_raw
+
+	efi_start_raw=${free_disksize}
+	(( efi_end_raw=${efi_start_raw} + ${efi_end_raw_raw} ))
+	efi_start=${efi_start_raw}GiB
+	efi_end=${efi_end_raw}GiB
+	(( last_partition=$(lsblk | grep $DISK | wc -l) - 1 ))
+	(( swap_partition=$(lsblk | grep $DISK | wc -l) - 2 ))
+
+	[[ "$DISK" == nvme* ]] && p="p"
+
+	if [[ "$efi_size" -gt 2357166592 ]]; then
+                echo " --- we have enought place for native dualboot --- "
+
+        	parted /dev/$DISK -- set 1 boot on
+        else
+               echo " --- we must create new boot partition --- "
+		parted /dev/$DISK -- mkpart ESP fat32 ${efi_start} ${efi_end}
+		efi_num=$(( $(lsblk | grep $DISK | wc -l) - 1 ))
+		parted -- set ${efi_num} boot on
+		mkfs.fat -F32 /dev/${DISK}${p}${efi_num}
+        fi
+
+	if [[ "${swap}" -ne 0 ]]; then
+        	swap_start=${efi_end}
+                swap_end=$(( ${swap} + ${efi_end_raw} ))
+		parted /dev/$DISK -- mkpart linux-swap ${swap_start} ${swap_end}
+		parted /dev/$DISK -- mkpart primary ext4 ${swap_end} 100%
+		mkswap /dev/${DISK}${p}${swap_partition}
+		mkfs.ext4 /dev/${DISK}${p}${last_partition}
+		mount /dev/${DISK}${p}${last_partition} /mnt
+		mkdir -p /mnt/boot
+		mount /dev/${DISK}${p}${efi_num} /mnt/boot
+
+        elif [[ "${swap}"' -eq 0 ]]; then
+                parted /dev/$DISK -- mkpart primary ext4 ${efi_end} 100%
+		mkfs.ext4 /dev/${DISK}${p}${last_partition}
+		mount /dev/${DISK}${p}${last_partition} /mnt
+		mkdir -p /mnt/boot
+		mount /dev/${DISK}${p}${efi_num} /mnt/boot
+                echo " --- swap will not created --- "
+	fi
+
+
+}
+
+lsblk -f
+read -p " --- what disk you will use? (1/2/3...) --- \n --- " disk_choise
 DISK=$(lsblk -dno NAME | awk -v n="$disk_choise" 'NR==n')
 echo " --- you selected $DISK --- "
-free_disksize_raw=$(sudo parted /dev/$DISK unit GiB print free | grep "Free Space" | tail -n1 | awk '{print $3}')
-free_disksize=${free_disksize_raw%GiB}
-efi_size=$(lsblk -nb -o NAME,FSTYPE,FSSIZE,FSAVAIL /dev/$DISK | awk '$2 == "vfat" {print $4}')
-efi_size_echo=$(lsblk -n -o NAME,FSTYPE,FSSIZE,FSAVAIL /dev/$DISK | awk '$2 == "vfat" {print $4}')
+
+(( efi_partition_size_raw = free_disksize + 1 ))
+
+parted_start=${free_disksize}GiB
+echo " --- parted_start - ${parted_start}"
+
 echo "$efi_size_echo"
-read -p " --- do you wanna dual boot with windows? yes/no (handwrite) --- " dualboot
+read -p " --- do you wanna dual boot with windows? yes/no (handwrite) --- \n --- " dualboot
+read -p " -- how mush swap you want 0-20 (in GiB) --- \n --- write only number --- \n --- if you dont want swap, then press 0 \n --- " swap
 echo "$free_disksize_raw"
 if [[ "$free_disksize" -lt 10 ]]; then
-	read -p " --- you have less than 10GiB free space, you sure to continue installing? yes/no (handwriting) " free_space
+	read -p " --- you have less than 10GiB free space, you sure to continue installing? yes/no (handwriting) --- \n --- " free_space
 	if [[ "$free_space" -eq "yes" ]]; then
 		echo " -- ok, good luck ))) --- "
 	elif [[ "$free_space" -eq "no" ]]; then
@@ -99,23 +160,13 @@ if [[ "$free_disksize" -lt 10 ]]; then
 	fi
 fi
 
-if [[ "dualboot" -eq "yes" ]]; then
-	read -p " --- windows already installed (on this disk)? yes/no (handwrite) --- " win
-	if [[ "$win" -eq "yes" ]]; then
-		echo " --- you must have unallocated space --- "
-		if [[ "$efi_size" -gt 2357166592 ]]; then
-			echo " --- we have enought place for native dualboot --- "
-		else
-			echo " --- we must create new boot partition --- "
-		fi
-	elif [[ "$win" -eq "no" ]]; then
-		echo " --- so we erase all disk (print \"no\" if you dont want)" erase
-			if [[ "$erase" -eq "no" ]]; then
-				echo " --- so we stop installing --- "
-				exit 1
-			else
-				echo " --- erasing disk ... --- "
-			fi
-	fi
-
+if [[ "$dualboot" == "yes" ]]; then
+	echo " --- you selected dualboot --- "
+	#dualboot()
+elif [[ "$dualboot" == "no" ]]; then
+	echo " --- so we will erase all disk --- \n if you dont want erase disk write \"no\" --- \n --- " erase
+	[[ "$erase" == "no" ]] && exit 1
+	#clear_disk()
+else
+	echo " --- you writed wrong --- \n --- example input \"yes\" or \"no\" "
 fi

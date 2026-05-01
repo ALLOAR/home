ce8fe45d0ff78f4f01a85a3f5d963498bbe5d93b
ALLOAR <evgenzbiranik@gmail.com>
2026-05-01 22:55:16 +0200

    done a big work now i need end a solo boot func, other work good

--- install/disk.sh
+++ install/disk.sh
@@ -19,12 +19,13 @@ echo " --- you selected "${DISK}" --- "
 # Функция запоминаея количества свободного места на диске
 # Нуждаеться в доработке ибо свободных мест может быть несколько и пользователю нужно дать возможность выбора
 # Формат - 100 (единици в GiB)
-# Нуждаеться в доработке
+# НЕ Нуждаеться в доработке - я посчитал что подобный функционал должен быть реализован в других функциях
 function free_disksize() {
-
-    
   free_disksize_raw=$(parted -m /dev/$DISK unit GiB print free | awk -F: '$5=="free;" && $4!="0.00GiB" {print $4}')
   free_disksize=${free_disksize_raw%GiB}
+	if [[ "${free_disksize}" -lt "15" ]]; then
+		low_space
+	fi
 }
 
 # Функция проверки пустой диск или же нет сравнивает количество строк с диском 
@@ -44,10 +45,11 @@ function delete_partition() {
 	lsblk | grep "${DISK}"
 	read -p $' --- what partition you wanna delete 1/2/3... --- ' delete_partition
 	parted /dev/"${DISK}" rm "${delete_partition}"
-	echo " --- succed --- "
+	echo " --- succed --- \n"
 }
 
-# Функция удаляет всю  информацию в диске по средство пересоздания таблици разделов
+# Функция удаляет всю  информацию в диске по ср
+# едство пересоздания таблици разделов
 function erase_disk() {
 	parted /dev/"${DISK}" mklabel gpt
 }
@@ -55,13 +57,20 @@ function erase_disk() {
 # функция выбирает на какой разддел vfat (boot) будет установлена система 
 # Сначала проверяет количество таких разделов, если один то функция его запоминает 
 # Если разделов больше 1 то функция справшивает на какой из них будет установленна система 
-# Нуждаеться в доработке, большой
+# Трогать первую строку с переменной vfat не стоит, ибо она пусть и тупо однако считает общее количество  vfat разделов, в пользователь можно установть
+# свою систему на раздел другого диска (пусть это и не всегда хорошо, свои применения имеет)
+# НЕ Нуждаеться в доработке, все в целом то работает хорошо, vfat_end необходим для создания других разделов если диск пуст или сразу после vfat есть пустое место
 function vfat() { 
 	vfat=$(lsblk -n -o FSTYPE | grep vfat | awk '{print $1}' | awk '{print NR}')
 	if [[ "${vfat}" -eq 1 ]]; then  
 		lsblk -f
 		read -p $' --- You have vfat partition, do you want install you OS on that (recomendet) --- \n  --- (handwritin) yes/no ---  ' vfat_choise
-		vfat_partition=$(lsblk -n -l -o NAME,FSTYPE,TYPE | awk '$3=="part" && $2=="vfat" {print $1}')
+		if [[ "${vfat_choise}" == "no" ]]; then
+			vfat_partition="new"
+			return
+		else
+		vfat_partition=$(lsblk -n -l -o NAME,FSTYPE,TYPE | awk '$3=="part" && $2=="vfat" {print $1}') # - output like nvme0n1p1
+		fi
 	fi
 	if [[ "${vfat}" -gt 1 ]]; then
 		lsblk -n -l -o NAME,FSTYPE,TYPE | awk '$3=="part" && $2=="vfat" {print ++n,$1,$2}'	
@@ -74,36 +83,61 @@ function vfat() {
 # Функция проверят достаточно ли места на диске
 # Если места недостаточно то предлагает один из 3 вариантов
 # Удалить раздел, очистить полностью диск, выбрать другой диск 
-# Нуждается в доработке
+# НЕ Нуждается в доработке
 function low_space() {
 	read -p " --- You dont have enought place for instalation (less than 15GiB) you sure to continue? \n --- yes/no (handwriting) --- " low_space  
-	if [[ "${low_space}" == "yes" ]]; then echo " --- ok, good luck) --- "; fi
+	if [[ "${low_space}" == "yes" ]]; then echo " --- ok, good luck) --- ";
 	elif [[ "${low_space}" == "no" ]]; then
-		echo " --- we can delete partition erase entire disk or change disk --- "
-		read -p " --- 1 - delete partition --- \n --- 2 - erase entire disk --- \n --- 3 - change disk --- \n (write 1/2/3) --- " new_begining
-		if [[ "${new_begining}" -eq 1 ]]; then delete_partition; fi
-		if [[ "${new_begining}" -eq 2 ]]; then erase_disk; fi
+		echo " --- we can delete partition, erase entire disk or change disk --- "
+		read -p " --- 1 - delete partition --- \n --- 2 - erase entire disk --- \n --- 3 - change disk --- \n --- (write 1/2/3) --- " new_begining
+		if [[ "${new_begining}" -eq 1 ]]; then delete_partition disk_space; fi
+		if [[ "${new_begining}" -eq 2 ]]; then erase_disk disk_space; fi
 		if [[ "${new_begining}" -eq 3 ]]; then select_disk disk_space; fi
 	fi
 }
 
-# Функция проверяет свободное место на диске и если его недостаточно то
-# Перенаправляет на другую функцию которая должна решить эту проблему
-function disk_space() {
-	if [[ "${free_disksize}"-gt 15 ]]; then echo " --- we have enought place for instaling --- ";
-	else low_space; fi
-}
+function clear_install() {
+  
+  echo "[1/5] Разметка /dev/$DISK..."
+  # Создание таблицы разделов GPT
+  parted /dev/$DISK -- mklabel gpt
+  # EFI-раздел (ESP)
+  parted /dev/$DISK -- mkpart ESP fat32 1MiB 512MiB
+  parted /dev/$DISK -- set 1 boot on
+  # Основной раздел ext4
+  parted /dev/$DISK -- mkpart primary ext4 512MiB 100%
+  echo "[2/5] Форматирование разделов..."
+  # Форматирование EFI
+  mkfs.fat -F32 -n boot /dev/${DISK}${p}1
+  # Форматирование ext4
+  mkfs.ext4 -L nixos /dev/${DISK}${p}2
 
+  echo "[3.5/5] Монтирование разделов..."
+  mount ${DISK}${p}2 /mnt
+  mkdir -p /mnt/boot
+  mount ${DISK}${p}1 /mnt/boot
+
+
+}
 # Черновой вариант функции для boot на чистом диске 
 # Нуждается в доработке
 function solo_boot() {
-	vfat
-	disk_end_start=$(parted -m /dev/"${DISK}" print | awk 'END {print}' | awk -F: '{print $3}')
-	if [[ "${vfat_choise}" == "yes" ]]; then
-		parted /dev/"${DISK}" mkpart "${disk_end_start}" 100%
-		disk_end_end=$(parted -m /dev/"${DISK}" print | awk 'END {print}' | awk -F: '{print $3}')
-		mkfs.ext4 /dev/"${DISK}" 
-# Необходимо решить проблему возможности алокации разделов, когда свободное место окружено двумя другими разделами, или же свободных мест несколько
+	select_disk
+	free_disksize
+	if_disk_clear
+
+	if [[ ${disk_clear} = "0" ]]; then
+		clear_install
+	else
+		read -p $" --- you disk have some partitions, you wanna install near other partitions? --- \n --- yes/no (handwrite) --- " near_install
+		if [[ "${near_install}" == "yes" ]]; then
+			# Here i will install system just on free space
+		else
+			echo " --- so me should remove some partitions or entire disk ---"
+			read -p $" --- remove partition - 1, erase disk - 2 --- " decidion
+			if
+	fi
+
 }
 
 
